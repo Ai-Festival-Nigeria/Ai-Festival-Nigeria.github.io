4f3a889f1aa694d97c37ca58649dcd22214daa4b
“Precious <precious@chukundah.com>
2020-02-12 19:59:13 +0100

    Full boiler-plate

---
+++ index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="Chetalab is a full service product engineering lab; We work with ambitious 
+		teams to design, build and ship digital products and services that make news.">
+		<meta name="robots" content="index, follow">
+		<meta name="keywords" content="tech, technology companies in nigeria, software companies
+		in port harcourt">
+		<meta name="theme-color" content="#ffffff">
+
+		<!-- Facebook OpenGraph -->
+		<meta property="og:type" content="website">
+		<meta property="og:locale" content="en_us">
+		<meta property="og:url" content="https://chetalab.com">
+		<meta property="og:title" content="Sogrado | We brand. We design. We animate.">
+		<meta property="og:description" content="Chetalab is a full service product engineering lab; We work with ambitious 
+		teams to design, build and ship digital products and services that make news.">
+		<meta property="og:type" content="website" />
+		<meta property="og:locale" content="en_US" />
+		<!-- <meta property="og:image" content="https://sogrado.com/assets/img/og_image.jpg"> -->
+		<meta property="og:image:type" content="image/png">
+		<meta property="og:image:width" content="1200">
+		<meta property="og:image:height" content="630">
+		<meta property="og:site_name" content="https://www.facebook.com/chigisoft">
+		<meta property="og:see_also" content="https://twitter.com/chigisoft">
+		<meta property="og:see_also" content="https://linkedin.com/company/chigisoft">
+		
+		<!-- Twitter Card -->
+		<meta name="twitter:card" content="summary_large_image">
+		<meta name="twitter:site" content="@chigisoft">
+		<meta name="twitter:creator" content="@chigisoft">
+		<meta name="twitter:title" content="Sogrado | We brand. We design. We animate.">
+		<meta name="twitter:description" content="Chetalab is a full service product engineering lab; We work with ambitious 
+		teams to design, build and ship digital products and services that make news.">
+		<!-- <meta name="twitter:image" content="https://chigisoft.com/assets/img/og_image.jpg"> -->
+
+		<!-- Favicon -->
+		<link rel="apple-touch-icon" sizes="180x180" href="img/iconmain_logo.png">
+		<link rel="icon" type="image/png" href="assets/img/main_logo.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="assets/img/main_logo.png" sizes="16x16">
+		<link rel="mask-icon" href="assets/img/main_logo.png" color="#5bbad5">
+		<link rel="shortcut icon" type="image/x-icon" href="assets/img/main_logo.png">
+		<title>Chetalab | Product engineering studio.</title>
+
+		<!-- Adobe fonts: Interstate & Museo Sans -->
+		<link rel="stylesheet" href="https://use.typekit.net/iav4obt.css">
+
+		<link rel='stylesheet' type='text/css' media='screen' href='css/bootstrap.min.css'>
+		<link rel='stylesheet' type='text/css' media='screen' href='css/main.css'>
+		<link rel='stylesheet' type='text/css' media='screen' href='css/style.css'>
+
+		<link href="https://fonts.googleapis.com/css?family=Lato:700,900&display=swap" rel="stylesheet">
+		<link rel="icon" href="img/icons/favicon.png" type="image/png">
+	</head>
+
+	<body>
+		<?php include "include/header.php"?>
+
+		<section id="intro" class="bg-ai-blue">
+			<div class="container">
+				<div class="row align-items-end">
+					<div class="col-12 col-lg-7 hero-text">
+
+						<div class="mb-0 text-white sub-title">Data-Driven Generation</div>
+						<div class="title mt-0 mb-4 text-white">
+							ai festival 🇳🇬
+						</div>
+
+						<a href="#" class="btn btn-primary btn-lg">Register to attend</a>
+					</div>
+
+					<div class="col-12 col-lg-5 d-none d-md-block">
+						<img src="img/product-bot.svg" alt="" class="img-fluid">
+					</div>
+
+				</div>
+			</div>
+		</section>
+
+		<!-- Event philosophy Section  -->
+		<section id="eventPhilosophy" class="section-padding bg-ai-dblue">
+			<div class="container">
+				<div class="row">
+					...
+				</div>
+			</div>
+		</section>
+
+		<!-- Event summary Section  -->
+		<section id="eventSummary" class="section-padding bg-lgray">
+			<div class="container">
+				<div class="row">
+					...
+				</div>
+			</div>
+		</section>
+
+		<!-- Event features Section  -->
+		<section id="eventFeatures" class="section-padding">
+			<div class="container">
+				<div class="row">
+					...
+				</div>
+			</div>
+		</section>
+
+		<!-- Call to action Section  -->
+		<section id="cta" class="section-padding bg-ai-blue">
+			<div class="container">
+				<div class="row">
+					...
+				</div>
+			</div>
+		</section>
+
+		<?php include "include/footer.php"?>
+	</body>
+</html>
